654bb5d22e17bdb7f89a103285fd0a86b245b855
meta4r <alalemns@gmail.com>
2023-02-09 23:55:08 +0200

    Create hello_world.asm

---
+++ hello_world.asm
@@ -0,0 +1,18 @@
+section .data
+    msg db 'Hello, World!', 0   ; null-terminated string
+
+section .text
+    global _start
+
+_start:
+    ; write string to stdout
+    mov eax, 4      ; system call number (sys_write)
+    mov ebx, 1      ; file descriptor (stdout)
+    mov ecx, msg    ; pointer to message
+    mov edx, 13     ; length of message
+    int 0x80        ; make system call
+
+    ; exit program
+    mov eax, 1      ; system call number (sys_exit)
+    xor ebx, ebx    ; exit status code
+    int 0x80        ; make system call
